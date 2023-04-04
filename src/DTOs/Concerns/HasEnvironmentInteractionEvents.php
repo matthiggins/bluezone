011e710c9a337874a38e0bf23e7ee603d86189e7
Matt Higgins <matt@creativestasis.com>
2023-04-04 11:56:12 -0500

    feat: add additional event aggregation for match telemetry

---
+++ src/DTOs/Concerns/HasEnvironmentInteractionEvents.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bluezone\DTOs\Concerns;
+
+trait HasEnvironmentInteractionEvents
+{
+    /**
+     * Player take bluezone damage events
+     */
+    public function takeBluezoneDamageEvents(): \Illuminate\Support\Collection
+    {
+        return $this->takeDamageEvents()->filter(fn ($e) => ! isset($e->attacker) && $e->damageTypeCategory == 'Damage_BlueZone');
+    }
+}
